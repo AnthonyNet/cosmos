a5a23fe013fa6d6aa4b66d840c93f94a07a789d5
AnthonyZet <zejda.anthony@gmai.com>
2023-01-04 13:27:54 +0100

    hero section css saved in variable

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,31 +1,46 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from 'next/image';
+import Image from "next/image";
 
 import styles from "../styles";
 import { slideIn, textVariant } from "../components/animations/motion";
 
+const style_ = {
+  heroHeading:
+    "font-bold lg:text-[144px] md:text-[100px] text-[10vw] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase font ",
+  div_Image:
+    "absolute w-full h-[300px] hero-gradient rounded-tl-[140px] rounded-r-[80px]  z-[0] -top-[30px]",
+  Image:
+    "w-full  h-[500px] object-cover rounded-tl-[140px] rounded-r-[50px] rounded-bl-lg z-10 relative",
+};
+
 const Hero = () => (
-  <section className={`${styles.yPaddings} h-screen w-screen sm:pl-16 pl-6 hero-gradient `}>
+  <section
+    className={`${styles.yPaddings} h-screen w-screen sm:px-16 px-6 hero-gradient `}
+  >
     <motion.div
-    
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amout: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <div className="flex justify-center items-center flex-col relative z-10 ">
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading + 'mt-20 md:mt-10 lg:mt-0 hero_Headline'}>
+      <div className={styles.flexCenter + " flex-col relative z-10 "}>
+        <motion.h1
+          variants={textVariant(1.1)}
+          className={
+            style_.heroHeading + "mt-20 md:mt-10 lg:mt-0 hero_Headline"
+          }
+        >
           Cosmos
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-            <h1 className={styles.heroHeading +'hero_Headline mb-4'}>Adventure</h1>
-        
-        
+          <h1 className={style_.heroHeading + "hero_Headline mb-4"}>
+            Adventure
+          </h1>
         </motion.div>
       </div>
 
@@ -33,17 +48,14 @@ const Hero = () => (
         variants={slideIn("right", "tween", 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] rounded-r-[80px]  z-[0] -top-[30px]" />
-          <Image
-            src="/hero.bmp"
-            alt="cover"
-            height={1024} // Desired size with correct aspect ratio
-            width={860}
-            className="w-full  h-[500px] object-cover rounded-tl-[140px] rounded-r-[50px] rounded-bl-lg z-10 relative"
-          />
-
-       
-        
+        <div className={style_.div_Image} />
+        <Image
+          src="/hero.bmp"
+          alt="cover"
+          height={1024} // Desired size with correct aspect ratio
+          width={860}
+          className={style_.Image}
+        />
       </motion.div>
     </motion.div>
   </section>
--- styles/index.js
+++ styles/index.js
@@ -14,12 +14,9 @@ const styles = {
     navPadding: 'pt-[98px]',
     sectionScreen: 'w-screen h-screen',
   
-    // hero section
-    heroHeading:
-        'font-bold lg:text-[144px] md:text-[100px] text-[60px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase font ',
-    heroDText:
-        'md:w-[180px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[70px] border-white sm:mx-2 mx-[6px]',
-  };
+    
+}
+    
   
   export default styles;
   
